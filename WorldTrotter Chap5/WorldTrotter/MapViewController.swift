2b00e2530e07515d5a5a4b9e398cdc2e6da94670
Lee Junho <dangercloz@gmail.com>
2017-03-24 15:55:09 +0900

    Chapter5 Sample

---
+++ WorldTrotter Chap5/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Lee Junho on 2017. 3. 24..
+//  Copyright © 2017년 Lee Junho. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
