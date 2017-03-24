a5d9fe7634dab8c885b6a8e97f64d76574b98336
Lee Junho <dangercloz@gmail.com>
2017-03-24 14:37:33 +0900

    Chapter 3 Sample

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Lee Junho on 2017. 3. 24..
+//  Copyright © 2017년 Lee Junho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+////        view.addSubview(secondView)
+//        firstView.addSubview(secondView)
+//    }
+}
