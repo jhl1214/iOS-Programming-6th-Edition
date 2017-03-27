dc1ee0592b13cb73333a2d3415ab1f612cf014cb
Lee Junho <dangercloz@gmail.com>
2017-03-27 23:44:23 +0900

    Chapter 7 Bronze

---
+++ WorldTrotter Chap7 Bronze/ConversionViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Lee Junho on 2017. 3. 24..
+//  Copyright © 2017년 Lee Junho. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension ConversionViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard string.rangeOfCharacter(from: NSCharacterSet.letters) == nil else {
+            return false
+        }
+        
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+        let existingTextHasDecimalSeparator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
+        
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+extension ConversionViewController {
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+}
+
+extension ConversionViewController {
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+}
+
+extension ConversionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let calendar = Calendar.current
+        let component = calendar.component(.hour, from: Date())
+        if component > 18 || component < 9 {
+            view.backgroundColor = UIColor.lightGray
+        } else {
+            view.backgroundColor = UIColor.groupTableViewBackground
+        }
+    }
+}
